848e6f00fb045efe24b56dcbfd5f8c754f853865
VinceMack <sentinelvhast@gmail.com>
2023-04-06 11:21:25 -0700

    2 editor button to test labor order UI functions

--- Assets/editor/PawnInspector.cs
+++ Assets/editor/PawnInspector.cs
@@ -49,5 +49,22 @@ public class PawnEditor : Editor
 
             EditorGUILayout.EndVertical();
         }
+
+        EditorGUILayout.Space();
+
+        // Add buttons to move a random labor type up or down in priority
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Move Random Labor Type Up"))
+        {
+            LaborType randomLaborType = (LaborType)Random.Range(0, System.Enum.GetValues(typeof(LaborType)).Length);
+            pawn.MoveLaborTypeUpPriorityLevel(randomLaborType);
+        }
+
+        if (GUILayout.Button("Move Random Labor Type Down"))
+        {
+            LaborType randomLaborType = (LaborType)Random.Range(0, System.Enum.GetValues(typeof(LaborType)).Length);
+            pawn.MoveLaborTypeDownPriorityLevel(randomLaborType);
+        }
+        EditorGUILayout.EndHorizontal();
     }
 }
